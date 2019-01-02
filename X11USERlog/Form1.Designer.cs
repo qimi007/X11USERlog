a6c971ccce3c4e6b64706e3e9891499a817c7870
qimi <2231972074@qq.com>
2019-01-02 16:41:10 +0800

    完成UI交互及对接任务程序

--- X11USERlog/Form1.Designer.cs
+++ X11USERlog/Form1.Designer.cs
@@ -28,18 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tb_host = new System.Windows.Forms.TextBox();
-            this.tb_username = new System.Windows.Forms.TextBox();
             this.tb_passwd = new System.Windows.Forms.TextBox();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_host = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.btn_datadir = new System.Windows.Forms.Button();
             this.tb_datadir = new System.Windows.Forms.TextBox();
+            this.btn_datadir = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.btn_start = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
@@ -76,25 +77,27 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "1.MYSQL数据库连接";
             // 
-            // label2
+            // tb_passwd
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 10F);
-            this.label2.Location = new System.Drawing.Point(31, 34);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 14);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "数据库主机：";
+            this.tb_passwd.Location = new System.Drawing.Point(122, 104);
+            this.tb_passwd.Name = "tb_passwd";
+            this.tb_passwd.PasswordChar = '*';
+            this.tb_passwd.Size = new System.Drawing.Size(147, 26);
+            this.tb_passwd.TabIndex = 1;
             // 
-            // label3
+            // tb_username
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("宋体", 10F);
-            this.label3.Location = new System.Drawing.Point(31, 73);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(91, 14);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "用  户  名：";
+            this.tb_username.Location = new System.Drawing.Point(122, 67);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(147, 26);
+            this.tb_username.TabIndex = 1;
+            // 
+            // tb_host
+            // 
+            this.tb_host.Location = new System.Drawing.Point(122, 33);
+            this.tb_host.Name = "tb_host";
+            this.tb_host.Size = new System.Drawing.Size(147, 26);
+            this.tb_host.TabIndex = 1;
             // 
             // label4
             // 
@@ -106,27 +109,25 @@
             this.label4.TabIndex = 0;
             this.label4.Text = "密      码：";
             // 
-            // tb_host
+            // label3
             // 
-            this.tb_host.Location = new System.Drawing.Point(122, 33);
-            this.tb_host.Name = "tb_host";
-            this.tb_host.Size = new System.Drawing.Size(147, 26);
-            this.tb_host.TabIndex = 1;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10F);
+            this.label3.Location = new System.Drawing.Point(31, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 14);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "用  户  名：";
             // 
-            // tb_username
+            // label2
             // 
-            this.tb_username.Location = new System.Drawing.Point(122, 67);
-            this.tb_username.Name = "tb_username";
-            this.tb_username.Size = new System.Drawing.Size(147, 26);
-            this.tb_username.TabIndex = 1;
-            // 
-            // tb_passwd
-            // 
-            this.tb_passwd.Location = new System.Drawing.Point(122, 104);
-            this.tb_passwd.Name = "tb_passwd";
-            this.tb_passwd.PasswordChar = '*';
-            this.tb_passwd.Size = new System.Drawing.Size(147, 26);
-            this.tb_passwd.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10F);
+            this.label2.Location = new System.Drawing.Point(31, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 14);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "数据库主机：";
             // 
             // groupBox2
             // 
@@ -140,6 +141,14 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "2.加载数据";
             // 
+            // tb_datadir
+            // 
+            this.tb_datadir.Location = new System.Drawing.Point(34, 35);
+            this.tb_datadir.Name = "tb_datadir";
+            this.tb_datadir.ReadOnly = true;
+            this.tb_datadir.Size = new System.Drawing.Size(480, 26);
+            this.tb_datadir.TabIndex = 1;
+            // 
             // btn_datadir
             // 
             this.btn_datadir.Font = new System.Drawing.Font("宋体", 10F);
@@ -149,14 +158,7 @@
             this.btn_datadir.TabIndex = 0;
             this.btn_datadir.Text = "浏览";
             this.btn_datadir.UseVisualStyleBackColor = true;
-            // 
-            // tb_datadir
-            // 
-            this.tb_datadir.Location = new System.Drawing.Point(34, 35);
-            this.tb_datadir.Name = "tb_datadir";
-            this.tb_datadir.ReadOnly = true;
-            this.tb_datadir.Size = new System.Drawing.Size(480, 26);
-            this.tb_datadir.TabIndex = 1;
+            this.btn_datadir.Click += new System.EventHandler(this.btn_datadir_Click);
             // 
             // groupBox3
             // 
@@ -222,6 +224,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(720, 480);
             this.MinimumSize = new System.Drawing.Size(720, 480);
--- X11USERlog/Form1.cs
+++ X11USERlog/Form1.cs
@@ -6,6 +6,13 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+              
+
+
 
 namespace X11USERlog
 {
@@ -26,7 +33,183 @@ namespace X11USERlog
         //按下开始按钮
         private void btn_start_Click(object sender, EventArgs e)
         {
-            //System.Diagnostics.Process.Start(Application.StartupPath + “你的应用程序.exe”).WaitForExit()；
+            
+            //参数检测及解析系统检测
+            if (param_check() < 0)
+            {
+                return;
+            }
+
+            //数据库连接检测
+            if (mysql_link_check() < 0)
+            {
+                return;
+            }
+
+            //启动解析任务
+            //Thread t = new Thread(exec_decode_userlog);//创建了线程还未开启
+			//t.Start("start");//用来给函数传递参数，开启线程
+
+            string argvText = String.Format("uyehuser {0} {1}  {2} {3}", tb_host.Text, tb_username.Text, tb_passwd.Text, tb_datadir.Text);
+
+            BackgroundWorker doWorker = new BackgroundWorker();
+            doWorker.DoWork += exec_decode_userlog;
+            doWorker.WorkerReportsProgress = true;
+            doWorker.ProgressChanged += BGWorker_ProgressChanged;
+            doWorker.RunWorkerAsync(argvText);
+
+
+        }
+
+
+      
+
+        private void btn_datadir_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+               tb_datadir.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+        }
+
+
+
+
+        private void BGWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            BackgroundWorker bgWorker = sender as BackgroundWorker;
+            int type = 0;
+            type = e.ProgressPercentage;
+            //MessageBox.Show(""+ type);
+            if (type > 0)
+            {
+                com_focus_ctl(1);
+            }
+            else 
+            {
+                com_focus_ctl(0);
+            }
+            
+        }
+
+
+        private void exec_decode_userlog(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgWorker = sender as BackgroundWorker;
+            //string argvText = String.Format("uyehuser {0} {1}  {2} {3}",tb_host.Text, tb_username.Text, tb_passwd.Text, tb_datadir.Text);
+            string argvText = (string)e.Argument;
+            //System.Diagnostics.Process.Start(Application.StartupPath + "test.exe  " + argvText).WaitForExit();
+            //MessageBox.Show(Application.StartupPath + "\\test.exe  "+ argvText);
+
+            //参数输入失焦
+            //com_focus_ctl(0);
+            bgWorker.ReportProgress(0);
+            System.Diagnostics.Process.Start(Application.StartupPath + "\\usertool.exe" , argvText).WaitForExit();
+            MessageBox.Show("解析成功");
+            //参数输入获取焦点
+            //com_focus_ctl(1);
+            bgWorker.ReportProgress(1);
+
+        }
+
+
+        //参数控件焦点控制
+        private void com_focus_ctl(int type)
+        {
+            if (type > 0)
+            {
+                //获取焦点
+                tb_host.Enabled = true;
+                tb_username.Enabled = true;
+                tb_passwd.Enabled = true;
+                btn_datadir.Enabled = true;
+                btn_start.Enabled = true;
+            }
+            else
+            { 
+                //失去焦点
+                tb_host.Enabled = false;
+                tb_username.Enabled = false;
+                tb_passwd.Enabled = false;
+                btn_datadir.Enabled = false;
+                btn_start.Enabled = false;
+            }
+        }
+
+        
+
+        //输入参数检测及解析系统检测
+        private int param_check()
+        {
+            //数据库主机检测
+            if (tb_host.Text == "")
+            {
+                MessageBox.Show("请填写主机名", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -1;
+            }
+            //数据库用户名检测
+            if (tb_username.Text == "")
+            {
+                MessageBox.Show("请填写用户名", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -2;
+            }
+            //数据库密码检测
+            if (tb_passwd.Text == "")
+            {
+                MessageBox.Show("请填写密码", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -3;
+            }
+
+            //数据目录检测
+            if (tb_datadir.Text == "")
+            {
+                MessageBox.Show("请选择要解析的数据目录", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -4;
+            }
+
+            //工具是否存在检测
+            if (!File.Exists(Application.StartupPath + "\\usertool.exe"))
+            {
+                MessageBox.Show("解析系统不存在...", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -5;
+            }
+
+            return 0;
+
+        }
+
+
+        //数据库连接检测实现
+        private int mysql_link_check()
+        { 
+            string connStr = String.Format("server={0};user id={1}; password={2}; port={3}; database=uyehdb; pooling=false; charset=utf8",
+            tb_host.Text, tb_username.Text, tb_passwd.Text, 3306);
+            MySqlConnection mysqlconn;
+            int ret = 0;
+
+            try 
+             
+            {
+
+                mysqlconn = new MySqlConnection(connStr);
+
+                mysqlconn.Open();
+                //MessageBox.Show("连接数据库成功!");
+                
+                mysqlconn.Close();
+             
+            }
+            catch (MySqlException ex) 
+            {
+             
+                MessageBox.Show( "Error connecting to the server: " + ex.Message , "数据库连接失败", MessageBoxButtons.OK , MessageBoxIcon.Error);
+                ret = -1;
+            }
+             
+            return ret;
         }
     }
 }
+
+
